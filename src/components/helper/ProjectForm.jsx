242d99b7a6cb973f887f70527c9ca4feebf880d0
Kartik Singh <kartiksinghk2001@gmail.com>
2024-07-23 00:11:13 +0530

    bug fixed:unable to add new projects

--- src/components/helper/ProjectForm.jsx
+++ src/components/helper/ProjectForm.jsx
@@ -6,119 +6,114 @@ import { v4 as uniqueId } from "uuid";
 import { CreateProject } from "../../utils/Helpers";
 
 const ProjectForm = ({
-	closeForm,
-	project,
-	setProject,
-	editProject,
-	setEditForm,
+  closeForm,
+  project,
+  setProject,
+  editProject,
+  setEditForm,
 }) => {
-	const [currProject, setCurrProject] = useState(
-		editProject || new CreateProject()
-	);
-	const [error, setError] = useState(false);
-	const handleChange = (event, key) => {
-		setCurrProject({ ...currProject, [key]: event.target.value });
-	};
-	const save = () => {
-		const { projectName, techUsed, description } = currProject;
-		if (!projectName || !techUsed || !description) {
-			setError(true);
-		} else {
-			let newProjectSet;
-			if (editProject) {
-				newProjectSet = project.map((thisProject) => {
-					if (thisProject.key === editProject.key) {
-						return currProject;
-					}
-					return thisProject;
-				});
-			} else {
-				newProjectSet = [
-					...project,
-					{ ...currProject, key: uniqueId() },
-				];
-			}
-			localStorage.setItem("projects", JSON.stringify(newProjectSet));
-			setProject(newProjectSet);
-			setEditForm(new CreateProject());
-			closeForm();
-		}
-	};
-	const cancel = () => {
-		setEditForm(new CreateProject());
-		closeForm();
-	};
-	return (
-		<div>
-			<Input
-				label={"Project Name"}
-				required
-				value={currProject.projectName}
-				onChange={(event) => handleChange(event, "projectName")}
-				placeholder={"Food App"}
-			/>
-			<Input
-				label={"Tech Used"}
-				required
-				value={currProject.techUsed}
-				onChange={(event) => handleChange(event, "techUsed")}
-				placeholder={"HTML, CSS, JavaScript"}
-			/>
-			<div className='flex space-x-5'>
-				<Input
-					label={"Start Date"}
-					className={"grow"}
-					placeholder={"August 2022"}
-					value={currProject.startDate}
-					onChange={(event) => handleChange(event, "startDate")}
-				/>
-				<Input
-					label={"End Date"}
-					className={"grow"}
-					placeholder={"August 2025"}
-					value={currProject.endDate}
-					onChange={(event) => handleChange(event, "endDate")}
-				/>
-			</div>
-			<Textarea
-				label={"Description"}
-				required
-				placeholder={
-					"Separate each achievement with enter key ↵ \nExample:-\nDeveloped a REST API using FastAPI and PostgreSQL to store data from learning management systems. \nDeveloped a full-stack web application using Flask, React, PostgreSQL and Docker to analyze GitHub data."
-				}
-				value={currProject.description}
-				onChange={(event) => handleChange(event, "description")}
-			/>
-			<div className='flex space-x-5'>
-				<Input
-					label={"Github Link"}
-					className={"grow"}
-					placeholder={""}
-					value={currProject.githubLink}
-					onChange={(event) => handleChange(event, "githubLink")}
-				/>
-				<Input
-					label={"Live Link"}
-					className={"grow"}
-					placeholder={""}
-					value={currProject.liveLink}
-					onChange={(event) => handleChange(event, "liveLink")}
-				/>
-			</div>
-			<div className='flex space-x-6 justify-center'>
-				<Button
-					onClick={save}
-					className={" bg-green-600 text-white shadow-md"}>
-					Save
-				</Button>
-				<Button onClick={cancel} className={" shadow-md"}>
-					Cancel
-				</Button>
-			</div>
-			<p className='text-red-500 mt-10 text-center'>
-				{error && "Some fields are missing"}
-			</p>
-		</div>
-	);
+  const [currProject, setCurrProject] = useState(
+    editProject || new CreateProject(),
+  );
+  const [error, setError] = useState(false);
+  const handleChange = (event, key) => {
+    setCurrProject({ ...currProject, [key]: event.target.value });
+  };
+  const save = () => {
+    const { projectName, techUsed, description } = currProject;
+    if (!projectName || !techUsed || !description) {
+      setError(true);
+    } else {
+      let newProjectSet;
+      if (editProject) {
+        newProjectSet = project.map((thisProject) => {
+          if (thisProject.key === editProject.key) {
+            return currProject;
+          }
+          return thisProject;
+        });
+      } else {
+        newProjectSet = [...project, { ...currProject, key: uniqueId() }];
+      }
+      localStorage.setItem("projects", JSON.stringify(newProjectSet));
+      setProject(newProjectSet);
+      setEditForm(null);
+      closeForm();
+    }
+  };
+  const cancel = () => {
+    setEditForm(new CreateProject());
+    closeForm();
+  };
+  return (
+    <div>
+      <Input
+        label={"Project Name"}
+        required
+        value={currProject.projectName}
+        onChange={(event) => handleChange(event, "projectName")}
+        placeholder={"Food App"}
+      />
+      <Input
+        label={"Tech Used"}
+        required
+        value={currProject.techUsed}
+        onChange={(event) => handleChange(event, "techUsed")}
+        placeholder={"HTML, CSS, JavaScript"}
+      />
+      <div className="flex space-x-5">
+        <Input
+          label={"Start Date"}
+          className={"grow"}
+          placeholder={"August 2022"}
+          value={currProject.startDate}
+          onChange={(event) => handleChange(event, "startDate")}
+        />
+        <Input
+          label={"End Date"}
+          className={"grow"}
+          placeholder={"August 2025"}
+          value={currProject.endDate}
+          onChange={(event) => handleChange(event, "endDate")}
+        />
+      </div>
+      <Textarea
+        label={"Description"}
+        required
+        placeholder={
+          "Separate each achievement with enter key ↵ \nExample:-\nDeveloped a REST API using FastAPI and PostgreSQL to store data from learning management systems. \nDeveloped a full-stack web application using Flask, React, PostgreSQL and Docker to analyze GitHub data."
+        }
+        value={currProject.description}
+        onChange={(event) => handleChange(event, "description")}
+      />
+      <div className="flex space-x-5">
+        <Input
+          label={"Github Link"}
+          className={"grow"}
+          placeholder={""}
+          value={currProject.githubLink}
+          onChange={(event) => handleChange(event, "githubLink")}
+        />
+        <Input
+          label={"Live Link"}
+          className={"grow"}
+          placeholder={""}
+          value={currProject.liveLink}
+          onChange={(event) => handleChange(event, "liveLink")}
+        />
+      </div>
+      <div className="flex space-x-6 justify-center">
+        <Button onClick={save} className={" bg-green-600 text-white shadow-md"}>
+          Save
+        </Button>
+        <Button onClick={cancel} className={" shadow-md"}>
+          Cancel
+        </Button>
+      </div>
+      <p className="text-red-500 mt-10 text-center">
+        {error && "Some fields are missing"}
+      </p>
+    </div>
+  );
 };
 export default ProjectForm;
--- src/components/helper/WorkForm.jsx
+++ src/components/helper/WorkForm.jsx
@@ -6,101 +6,93 @@ import { v4 as uniqueId } from "uuid";
 import { CreateJob } from "../../utils/Helpers";
 
 const WorkForm = ({ closeForm, setEditForm, work, setWork, editJob }) => {
-	const [job, setJob] = useState(editJob || new CreateJob());
-	const [error, setError] = useState(false);
-	const handleChange = (event, key) => {
-		setError(false);
-		setJob({ ...job, [key]: event.target.value });
-	};
-	const save = () => {
-		const { companyName, position, startDate, endDate, description } = job;
-		if (
-			!companyName ||
-			!position ||
-			!startDate ||
-			!endDate ||
-			!description
-		) {
-			setError(true);
-		} else {
-			let newWorkSet;
-			if (editJob) {
-				newWorkSet = work.map((currJob) => {
-					if (currJob.key === editJob.key) {
-						return job;
-					}
-					return currJob;
-				});
-			} else {
-				newWorkSet = [...work, { ...job, key: uniqueId() }];
-			}
-			localStorage.setItem("experience", JSON.stringify(newWorkSet));
-			setWork(newWorkSet);
-			setEditForm(new CreateJob());
-			closeForm();
-		}
-	};
-	const cancel = () => {
-		setEditForm(new CreateJob());
-		closeForm();
-	};
-	return (
-		<div>
-			<Input
-				label={"Company"}
-				value={job.companyName}
-				required
-				onChange={(event) => handleChange(event, "companyName")}
-				placeholder={"Mircrosoft"}
-			/>
-			<Input
-				label={"Position"}
-				value={job.position}
-				required
-				onChange={(event) => handleChange(event, "position")}
-				placeholder={"SDE-I"}
-			/>
-			<div className='flex space-x-5'>
-				<Input
-					label={"Start Date"}
-					className={"grow"}
-					placeholder={"August 2022"}
-					required
-					value={job.startDate}
-					onChange={(event) => handleChange(event, "startDate")}
-				/>
-				<Input
-					label={"End Date"}
-					className={"grow"}
-					placeholder={"August 2025"}
-					required
-					value={job.endDate}
-					onChange={(event) => handleChange(event, "endDate")}
-				/>
-			</div>
-			<Textarea
-				label={"Description"}
-				required
-				placeholder={
-					"Separate each achievement with enter key ↵ \nExample:-\nDeveloped a REST API using FastAPI and PostgreSQL to store data from learning management systems. \nDeveloped a full-stack web application using Flask, React, PostgreSQL and Docker to analyze GitHub data."
-				}
-				value={job.description}
-				onChange={(event) => handleChange(event, "description")}
-			/>
-			<div className='flex space-x-6 justify-center'>
-				<Button
-					onClick={save}
-					className={" bg-green-600 text-white shadow-md"}>
-					Save
-				</Button>
-				<Button onClick={cancel} className={" shadow-md"}>
-					Cancel
-				</Button>
-			</div>
-			<p className='text-red-500 mt-10 text-center'>
-				{error && "Some fields are missing"}
-			</p>
-		</div>
-	);
+  const [job, setJob] = useState(editJob || new CreateJob());
+  const [error, setError] = useState(false);
+  const handleChange = (event, key) => {
+    setError(false);
+    setJob({ ...job, [key]: event.target.value });
+  };
+  const save = () => {
+    const { companyName, position, startDate, endDate, description } = job;
+    if (!companyName || !position || !startDate || !endDate || !description) {
+      setError(true);
+    } else {
+      let newWorkSet;
+      if (editJob) {
+        newWorkSet = work.map((currJob) => {
+          if (currJob.key === editJob.key) {
+            return job;
+          }
+          return currJob;
+        });
+      } else {
+        newWorkSet = [...work, { ...job, key: uniqueId() }];
+      }
+      localStorage.setItem("experience", JSON.stringify(newWorkSet));
+      setWork(newWorkSet);
+      setEditForm(null);
+      closeForm();
+    }
+  };
+  const cancel = () => {
+    setEditForm(new CreateJob());
+    closeForm();
+  };
+  return (
+    <div>
+      <Input
+        label={"Company"}
+        value={job.companyName}
+        required
+        onChange={(event) => handleChange(event, "companyName")}
+        placeholder={"Mircrosoft"}
+      />
+      <Input
+        label={"Position"}
+        value={job.position}
+        required
+        onChange={(event) => handleChange(event, "position")}
+        placeholder={"SDE-I"}
+      />
+      <div className="flex space-x-5">
+        <Input
+          label={"Start Date"}
+          className={"grow"}
+          placeholder={"August 2022"}
+          required
+          value={job.startDate}
+          onChange={(event) => handleChange(event, "startDate")}
+        />
+        <Input
+          label={"End Date"}
+          className={"grow"}
+          placeholder={"August 2025"}
+          required
+          value={job.endDate}
+          onChange={(event) => handleChange(event, "endDate")}
+        />
+      </div>
+      <Textarea
+        label={"Description"}
+        required
+        placeholder={
+          "Separate each achievement with enter key ↵ \nExample:-\nDeveloped a REST API using FastAPI and PostgreSQL to store data from learning management systems. \nDeveloped a full-stack web application using Flask, React, PostgreSQL and Docker to analyze GitHub data."
+        }
+        value={job.description}
+        onChange={(event) => handleChange(event, "description")}
+      />
+      <div className="flex space-x-6 justify-center">
+        <Button onClick={save} className={" bg-green-600 text-white shadow-md"}>
+          Save
+        </Button>
+        <Button onClick={cancel} className={" shadow-md"}>
+          Cancel
+        </Button>
+      </div>
+      <p className="text-red-500 mt-10 text-center">
+        {error && "Some fields are missing"}
+      </p>
+    </div>
+  );
 };
 export default WorkForm;
